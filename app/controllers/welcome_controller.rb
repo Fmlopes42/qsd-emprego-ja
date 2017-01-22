fe39c7c7337fac0e4ff27bb23c05b8602ab3479d
Fernanda Monteiro <fmlopes42@gmail.com>
2017-01-22 12:29:24 -0200

    Add feature home with jobs

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @jobs = Job.all 
   end
 end
---
+++ spec/features/visitor_view_jobs_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+feature 'Visitor view jobs on home page' do
+  scenario 'successfully' do
+    Job.create(title: 'Vaga de Dev',
+               category: 'Desenvolvedor',
+               company: 'Campus Code',
+               description: 'Dev Junior Rails com ao menos um projeto',
+               location: 'São Paulo')
+
+    visit root_path
+
+    expect(page).to have_content('Vaga de Dev')
+    expect(page).to have_content('Campus Code')
+    expect(page).to have_content('São Paulo')
+  end
+
+  scenario 'and view a list of jobs' do
+    job = Job.create(title: 'Vaga de Dev',
+               category: 'Desenvolvedor',
+               company: 'Campus Code',
+               description: 'Dev Junior Rails com ao menos um projeto',
+               location: 'São Paulo')
+
+    another_job = Job.create(title: 'UX Senior',
+                             category: 'UX',
+                             company: 'Facebook',
+                             description: 'UX com experiência em redes sociais',
+                             location: 'São Paulo')
+    visit root_path
+
+    expect(page).to have_css('h1',text: job.title)
+    expect(page).to have_content(job.company)
+    expect(page).not_to have_content(job.description)
+    expect(page).to have_css('h1',text: another_job.title)
+    expect(page).to have_content(another_job.company)
+    expect(page).not_to have_content(another_job.description)
+  end
+end
